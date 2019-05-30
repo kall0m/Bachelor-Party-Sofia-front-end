83654ea60e4dc7c08b470307938bd00669c08e5b
kall0m <k.madjunov@gmail.com>
2019-05-30 14:44:43 +0300

    Admin component: add information about the count of activities, quotations, users and parties

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivityService } from '../activities/shared/activity.service';
+import { QuotationService } from '../quotations/shared/quotation.service';
+import { UserService } from '../users/shared/user.service';
+import { PartyService } from '../parties/shared/party.service';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  activitiesCount: number;
+  quotationsCount: number;
+  usersCount: number;
+  partiesCount: number;
+
+  constructor(private activityService: ActivityService,
+              private quotationService: QuotationService,
+              private userService: UserService,
+              private partyService: PartyService) { }
 
   ngOnInit() {
+    this.getActivitiesCount();
+    this.getQuotationsCount();
+    this.getUsersCount();
+    this.getPartiesCount();
   }
 
+  getActivitiesCount(): void {
+    this.activityService.getActivities()
+      .subscribe(activities => {
+        this.activitiesCount = activities.length;
+      });
+  }
+
+  getQuotationsCount(): void {
+    this.quotationService.getQuotations()
+      .subscribe(quotations => {
+        this.quotationsCount = quotations.length;
+      });
+  }
+
+  getUsersCount(): void {
+    this.userService.getAllUsers()
+      .subscribe(users => {
+        this.usersCount = users.length;
+      });
+  }
+
+  getPartiesCount(): void {
+    this.partyService.getParties()
+      .subscribe(parties => {
+        this.partiesCount = parties.length;
+      });
+  }
 }
